17a7cec9c20650282f86f59456ba672a0b35c577
Dhairya <shahdhairya101@gmail.com>
2024-02-02 23:09:07 -0800

    homework-day1 week4

--- homework-module-2/js/randomcodes.js
+++ homework-module-2/js/randomcodes.js
@@ -1,15 +1,16 @@
 function generateCode() {
-  var code = " ";
+  var code = "";
   var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890@#$";
 
   for (i = 1; i <= 8; i++) {
     var char = Math.random() * str.length;
     code += str.charAt(char);
   }
-
+  console.log("code", code);
   return code;
 }
 document.getElementById("codes").innerHTML = generateCode();
+
 
 function disableButton() {
   document.getElementById("submit").disabled = true;
